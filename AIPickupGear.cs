6316a06604283c32d3d4b3997470390455126cca
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2020-10-20 21:54:54 -0400

    Implement armor and alternate weapon acquisition

--- AIPickupGear.cs
+++ AIPickupGear.cs
@@ -35,6 +35,12 @@ namespace XRL.World.Parts.CleverGirl
             if (findBetterPrimaryWeapon(parts, currentCell)) {
                 return;
             }
+            if (findBetterArmor(parts, currentCell)) {
+                return;
+            }
+            if (findBetterAlternateWeapon(parts, currentCell)) {
+                return;
+            }
         }
 
         private bool findBetterPrimaryWeapon(List<BodyPart> parts, Cell currentCell) {
@@ -56,7 +62,6 @@ namespace XRL.World.Parts.CleverGirl
                 return false;
             }
 
-    
             var noEquip = ParentObject.GetPropertyOrTag("NoEquip");
             var noEquipList = string.IsNullOrEmpty(noEquip) ? null : new List<string>(noEquip.CachedCommaExpansion());
 
@@ -76,10 +81,104 @@ namespace XRL.World.Parts.CleverGirl
                 return false;
             }
 
-            ParentObject.pBrain.Think("I want that " + betterWeapon.DisplayNameOnlyStripped);
-            ParentObject.pBrain.PushGoal(new GoPickupGear(betterWeapon));
-            ParentObject.pBrain.PushGoal(new MoveTo(betterWeapon.CurrentCell));
+            GoGet(betterWeapon);
             return true;
+        }
+        private bool findBetterArmor(List<BodyPart> parts, Cell currentCell) {
+            var armors = currentCell.ParentZone
+                .FastFloodVisibility(currentCell.X, currentCell.Y, 30, "Armor", ParentObject)
+                .Where(go => ParentObject.HasLOSTo(go))
+                .ToList();
+            if (0 == armors.Count) {
+                return false;
+            }
+            
+            if (armors.Count > 1) {
+                armors.Sort(new Brain.GearSorter(ParentObject));
+            }
+
+            var noEquip = ParentObject.GetPropertyOrTag("NoEquip");
+            var noEquipList = string.IsNullOrEmpty(noEquip) ? null : new List<string>(noEquip.CachedCommaExpansion());
+
+            GameObject betterArmor = null;
+            foreach (var armor in armors) {
+                if (noEquipList?.Contains(armor.Blueprint) ?? false) {
+                    continue;
+                }
+                if (armor.HasPropertyOrTag("NoAIEquip")) {
+                    continue;
+                }
+                foreach (var part in parts) {
+                    if (armor.GetPart<Armor>().WornOn != part.Type) {
+                        continue;
+                    }
+                    if (ParentObject.pBrain.IsNewArmorBetter(armor, part.Equipped)) {
+                        betterArmor = armor;
+                        break;
+                    }
+                }
+                if (null != betterArmor) {
+                    break;
+                }
+            }
+
+            if (null == betterArmor) {
+                return false;
+            }
+
+            GoGet(betterArmor);
+            return true;
+        }
+        private bool findBetterAlternateWeapon(List<BodyPart> parts, Cell currentCell) {
+            var weapons = currentCell.ParentZone
+                .FastFloodVisibility(currentCell.X, currentCell.Y, 30, "MeleeWeapon", ParentObject)
+                .Where(go => go.HasTag("MeleeWeapon") && ParentObject.HasLOSTo(go))
+                .ToList();
+            if (0 == weapons.Count) {
+                return false;
+            }
+            
+            if (weapons.Count > 1) {
+                weapons.Sort(new Brain.WeaponSorter(ParentObject));
+            }
+
+            var noEquip = ParentObject.GetPropertyOrTag("NoEquip");
+            var noEquipList = string.IsNullOrEmpty(noEquip) ? null : new List<string>(noEquip.CachedCommaExpansion());
+
+            GameObject betterWeapon = null;
+            foreach (var weapon in weapons) {
+                if (noEquipList?.Contains(weapon.Blueprint) ?? false) {
+                    continue;
+                }
+                if (weapon.HasPropertyOrTag("NoAIEquip")) {
+                    continue;
+                }
+                foreach (var part in parts) {
+                    if ("Hand" != part.Type) {
+                        continue;
+                    }
+                    if (ParentObject.pBrain.IsNewWeaponBetter(weapon, part.Equipped)) {
+                        betterWeapon = weapon;
+                        break;
+                    }
+                }
+                if (null != betterWeapon) {
+                    break;
+                }
+            }
+
+            if (null == betterWeapon) {
+                return false;
+            }
+
+            GoGet(betterWeapon);
+            return true;
+        }
+
+        void GoGet(GameObject item) {
+            ParentObject.pBrain.Think("I want that " + item.DisplayNameOnlyStripped);
+            ParentObject.pBrain.PushGoal(new GoPickupGear(item));
+            ParentObject.pBrain.PushGoal(new MoveTo(item.CurrentCell));
         }
     }
 }
