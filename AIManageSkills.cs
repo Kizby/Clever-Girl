0ba4892f397888cd58d908006fbdd3cf8e60d9bf
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2020-10-24 19:52:59 -0400

    Add hook to learn skills on level up

--- AIManageSkills.cs
+++ AIManageSkills.cs
@@ -4,6 +4,7 @@ namespace XRL.World.Parts.CleverGirl
 {
     using System.Collections.Generic;
     using System.Linq;
+    using XRL.Rules;
     using XRL.UI;
     using XRL.World.CleverGirl;
     using XRL.World.Skills;
@@ -19,30 +20,86 @@ namespace XRL.World.Parts.CleverGirl
 
         // these skills don't make sense for followers
         static string[] IgnoreSkills = {
-            "CookingAndGathering",
-            "Customs",
-            "Survival",
+            "Cooking and Gathering",
+            "Customs and Folklore",
             "Tinkering",
-            "Firstaid_Setlimb",
-            "Discipline_FastingWay",
-            "Discipline_MindOverBody",
+            "Wayfaring",
+            "Set Limb",
+            "Fasting Way",
+            "Mind over Body",
         };
 
+        Random random = Stat.GetSeededRandomGenerator("Kizby_CleverGirl");
+
         public List<string> LearningSkills = new List<string>();
+
+        public override bool WantEvent(int ID, int cascade)
+        {
+            return ID == StatChangeEvent.ID;
+        }
+
+        public override bool HandleEvent(StatChangeEvent E) {
+            if ("SP" != E.Name) {
+                return true;
+            }
+            var budget = E.NewValue;
+            var pool = new List<Tuple<string, int, string>>();
+            var toDrop = new List<string>();
+            foreach (var skillName in LearningSkills) {
+                var skill = SkillFactory.Factory.SkillList[skillName];
+                bool hasAllPowers = true;
+                if (ParentObject.HasSkill(skill.Class)) {
+                    foreach (var power in skill.Powers.Values) {
+                        if (!ParentObject.HasSkill(power.Class)) {
+                            hasAllPowers = false;
+                            if (power.Cost > budget) {
+                                continue;
+                            }
+                            if (!power.MeetsRequirements(ParentObject)) {
+                                continue;
+                            }
+                            pool.Add(Tuple.Create(power.Class, power.Cost, power.Name));
+                        }
+                    }
+                } else {
+                    hasAllPowers = false;
+                    if (skill.Cost <= budget) {
+                        pool.Add(Tuple.Create(skill.Class, skill.Cost, skill.Name));
+                    }
+                }
+                if (hasAllPowers) {
+                    toDrop.Add(skillName);
+                }
+            }
+            // drop skills that are already complete
+            LearningSkills = LearningSkills.Except(toDrop).ToList();
+
+            if (0 == pool.Count) {
+                // nothing to learn
+                return true;
+            }
+
+            var which = pool.GetRandomElement(random);
+            ParentObject.AddSkill(which.Item1);
+            E.Stat.Penalty += which.Item2;
+
+            this.DidX("learn", which.Item3, "!", ColorAsGoodFor: this.ParentObject);
+            return true;
+        }
 
         public void Manage() {
             var skills = new List<string>(SkillFactory.Factory.SkillList.Count);
             var strings = new List<string>(SkillFactory.Factory.SkillList.Count);
             var keys = new List<char>(SkillFactory.Factory.SkillList.Count);
             foreach (var Skill in SkillFactory.Factory.SkillList.Values) {
-                if (IgnoreSkills.Contains(Skill.Class)) {
+                if (IgnoreSkills.Contains(Skill.Name)) {
                     continue;
                 }
-                skills.Add(Skill.Class);
+                skills.Add(Skill.Name);
                 var havePowers = 0;
                 var totalPowers = 0;
                 foreach (var Power in Skill.Powers.Values) {
-                    if (IgnoreSkills.Contains(Power.Class)) {
+                    if (IgnoreSkills.Contains(Power.Name)) {
                         continue;
                     }
                     if (ParentObject.HasSkill(Power.Class)) {
@@ -50,7 +107,7 @@ namespace XRL.World.Parts.CleverGirl
                     }
                     ++totalPowers;
                 }
-                var prefix = havePowers == totalPowers ? "*" : LearningSkills.Contains(Skill.Class) ? "+" : "-";
+                var prefix = havePowers == totalPowers ? "*" : LearningSkills.Contains(Skill.Name) ? "+" : "-";
                 strings.Add(prefix + " " + Skill.Name + ": " + havePowers + "/" + totalPowers);
                 keys.Add((char)('a' + keys.Count));
             }
