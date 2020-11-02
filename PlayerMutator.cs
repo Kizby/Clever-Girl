137368b5f91e32731ea71e86586849e76f22cc81
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2020-11-02 18:28:14 -0500

    Add the InteractListener on game load too so games can add the mod

--- PlayerMutator.cs
+++ PlayerMutator.cs
@@ -1,12 +1,25 @@
 using XRL;
+using XRL.Core;
 using XRL.World;
 
 [PlayerMutator]
+[HasCallAfterGameLoaded]
 public class CleverGirlPlayerMutator : IPlayerMutator
 {
     public void mutate(GameObject player)
     {
         // add our listener to the player when a New Game begins
         player.AddPart<XRL.World.Parts.CleverGirl.InteractListener>();
+    }
+
+    [CallAfterGameLoaded]
+    public static void GameLoadedCallback()
+    {
+        // Called whenever loading a save game
+        GameObject player = XRLCore.Core?.Game?.Player?.Body;
+        if (player != null)
+        {
+            player.RequirePart<XRL.World.Parts.CleverGirl.InteractListener>();
+        }
     }
 }
