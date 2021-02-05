61319017a6d7294d2454f3cde274069f9e98c888
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-02-05 10:27:23 -0500

    Add Utility.Roll helper function

--- Utility.cs
+++ Utility.cs
@@ -2,6 +2,7 @@ namespace XRL.World.CleverGirl {
     using System;
     using System.Collections.Generic;
     using System.Runtime.CompilerServices;
+    using XRL.Rules;
 
     public static class Utility {
         public static bool debug;
@@ -14,15 +15,24 @@ namespace XRL.World.CleverGirl {
 
         private static readonly Dictionary<string, Random> RandomDict = new Dictionary<string, Random>();
         public static Random Random(IPart part) {
+            var key = GetKey(part);
+            if (!RandomDict.ContainsKey(key)) {
+                MaybeLog("Creating Random " + key);
+                RandomDict[key] = Stat.GetSeededRandomGenerator(key);
+            }
+            return RandomDict[key];
+        }
+
+        public static int Roll(string Dice, IPart part) {
+            return Stat.Roll(Dice, GetKey(part));
+        }
+
+        private static string GetKey(IPart part) {
             var key = "Kizby_CleverGirl_" + part.GetType().Name;
             if (part.ParentObject != null) {
                 key += "_" + part.ParentObject.id;
             }
-            if (!RandomDict.ContainsKey(key)) {
-                MaybeLog("Creating Random " + key);
-                RandomDict[key] = Rules.Stat.GetSeededRandomGenerator(key);
-            }
-            return RandomDict[key];
+            return key;
         }
 
         public class InventoryAction {
