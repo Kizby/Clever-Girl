23579636053ce8fbcfe1989d4ea3aa4542555d13
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2022-07-17 10:23:50 -0400

    Add effects info; more consistent names; cleaner separation of fields

--- CompanionsTracker.cs
+++ CompanionsTracker.cs
@@ -5,6 +5,8 @@ namespace XRL.World.CleverGirl {
     using XRL.UI;
     using System.Collections.Immutable;
     using XRL.Rules;
+    using HarmonyLib;
+    using System.Reflection;
 
     [HasGameBasedStaticCache]
     public static class CompanionsTracker {
@@ -33,6 +35,7 @@ namespace XRL.World.CleverGirl {
                     anyNew = true;
                     var companion = GameObject.findById(companionId);
                     if (companion != null) {
+                        Leader.SetStringProperty(NAME_PROPERTY + companionId, CompanionName(companion));
                         InitializeCompanionsList(companion);
                     }
                 }
@@ -73,7 +76,7 @@ namespace XRL.World.CleverGirl {
                 var currentList = Leader.GetStringProperty(LIST_PROPERTY);
                 if (currentList?.Contains(Companion.id) != true) {
                     Leader.SetStringProperty(LIST_PROPERTY, currentList.IsNullOrEmpty() ? Companion.id : currentList + "," + Companion.id);
-                    Leader.SetStringProperty(NAME_PROPERTY + Companion.id, Companion.DisplayName);
+                    Leader.SetStringProperty(NAME_PROPERTY + Companion.id, CompanionName(Companion));
                 }
             }
         }
@@ -89,30 +92,64 @@ namespace XRL.World.CleverGirl {
             }
             var companionIds = The.Player.GetStringProperty(LIST_PROPERTY).Split(',').ToImmutableHashSet();
             var localCompanions = The.ActiveZone.FindObjects(obj => companionIds.Contains(obj.id));
-            localCompanions.Sort((a, b) => a.pRender.DisplayName.CompareTo(b.pRender.DisplayName));
+            localCompanions.Sort((a, b) => CompanionName(a).CompareTo(CompanionName(b)));
 
-            var lines = new string[companionIds.Count];
-            var index = 0;
+            var names = new List<string>();
+            var status = new List<string>();
+            var effects = new List<string>();
             foreach (var companion in localCompanions) {
-                var line = companion.DisplayName + ": ";
+                names.Add(CompanionName(companion));
                 if (!companion.IsVisible()) {
-                    line += The.Player.DescribeDirectionToward(companion.CurrentCell);
+                    status.Add("{{O|" + The.Player.DescribeDirectionToward(companion.CurrentCell) + "}}");
+                    effects.Add("");
                 } else {
-                    line += Strings.WoundLevel(companion);
+                    status.Add(Strings.WoundLevel(companion));
+                    var effectString = "";
+                    foreach (var effect in companion.Effects) {
+                        var description = effect.GetDescription();
+                        if (!description.IsNullOrEmpty()) {
+                            if (effectString.Length > 0) {
+                                effectString += ", ";
+                            }
+                            effectString += description;
+                        }
+                    }
+                    effects.Add(effectString);
                 }
-                lines[index++] = line;
             }
             foreach (var companionId in companionIds) {
                 if (localCompanions.Any(obj => obj.id == companionId)) {
                     continue;
                 }
-                var line = The.Player.GetStringProperty(NAME_PROPERTY + companionId);
-                lines[index++] = line;
+                if (The.Player.HasStringProperty(NAME_PROPERTY + companionId)) {
+                    names.Add(The.Player.GetStringProperty(NAME_PROPERTY + companionId));
+                    status.Add("");
+                    effects.Add("");
+                } else if (Utility.debug) {
+                    names.Add(companionId);
+                    status.Add("");
+                    effects.Add("");
+                }
+            }
+            var lines = new string[names.Count];
+            for (var i = 0; i < lines.Length; ++i) {
+                var line = names[i];
+                if (status[i].Length > 0) {
+                    line += " | " + status[i];
+                }
+                if (effects[i].Length > 0) {
+                    line += " | " + effects[i];
+                }
+                lines[i] = line;
+                Utility.MaybeLog(line);
             }
             var selected = 0;
             while (selected != -1) {
                 selected = Popup.ShowOptionList("Companions", lines, AllowEscape: true);
             }
         }
+
+        private static readonly PropertyInfo DisplayNameBaseProperty = AccessTools.Property(typeof(GameObject), "DisplayNameBase");
+        private static string CompanionName(GameObject Companion) => DisplayNameBaseProperty.GetValue(Companion) as string;
     }
 }
